51f525beba7dccbba1e80e9f29cbfcba71bee42b
Noelle Rolwes <nrolwes@gmail.com>
2018-08-09 22:31:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,8 +67,8 @@ function placeOrder(cardNumber) {
   if (!cardNumber){
     return `Sorry, we don't have a credit card on file for you.`;
   } else {
-    cart = [];
     var chargeReply = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+    cart = [];
     return chargeReply;
   }
   
