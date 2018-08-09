3807853c578128de80aba3cf6c7331440b4b6e3e
Noelle Rolwes <nrolwes@gmail.com>
2018-08-09 22:23:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,12 +63,13 @@ return `That item is not in your cart.`;
 
 
 function placeOrder(cardNumber) {
-  var chargeReply = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
   
   if (!cardNumber){
     return `Sorry, we don't have a credit card on file for you.`;
   } else {
+    var chargeReply = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
     cart = [];
     return chargeReply;
   }
 }
+
